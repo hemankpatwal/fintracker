f20936a7b5ccc5bec6354df2fcb9392f6a8e86a8
hemankpatwal <hemankpatwalcse@gmail.com>
2025-02-28 00:44:11 +0530

    feat: Add Login Page and SignUp Page

--- financetracker/finance/views.py
+++ financetracker/finance/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) 
+            return redirect('home')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'finance/signup.html', {'form': form})
+
+def home(request):
+    return render(request, 'finance/home.html')
